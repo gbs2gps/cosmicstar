b64b921ce89cf20f3ff62c7f056edfb94e936c56
gbs2gps <gbs2gps@virginia.edu>
2021-12-07 12:48:55 -0500

    stuff

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap5',
+
 ]
 
 MIDDLEWARE = [
@@ -125,5 +127,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 django_heroku.settings(locals())
