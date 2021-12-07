b85964c063008732afd53dc8a343e961bd6ff0ab
gbs2gps <gbs2gps@virginia.edu>
2021-12-07 13:05:21 -0500

    pip

--- mysite/settings.py
+++ mysite/settings.py
@@ -129,4 +129,4 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-django_heroku.settings(locals(), test_runner = False)
+#django_heroku.settings(locals(), test_runner = False)
