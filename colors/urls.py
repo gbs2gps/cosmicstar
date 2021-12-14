8949e3252d594b6119513a2b57723e562a7950a5
gbs2gps <gbs2gps@virginia.edu>
2021-12-13 19:29:58 -0500

    more edits

--- colors/urls.py
+++ colors/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('1', views.index1, name='index1'),
+    path('2', views.index2, name='index2'),
+    path('3', views.index3, name='index3'),
+    path('4', views.index4, name='index4'),
+    path('5', views.index5, name='index5'),
+
 ]
--- colors/views.py
+++ colors/views.py
@@ -5,4 +5,19 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'colors/colorstuff.html')
 
+def index1(request):
+    return render(request, 'colors/colorstuff1.html')
+
+def index2(request):
+    return render(request, 'colors/colorstuff2.html')
+
+def index3(request):
+    return render(request, 'colors/colorstuff3.html')
+
+def index4(request):
+    return render(request, 'colors/colorstuff4.html')
+
+def index5(request):
+    return render(request, 'colors/colorstuff5.html')
+
 # Create your views here.
