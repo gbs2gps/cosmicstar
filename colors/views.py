69c740a3d68b20ef5561c95454b28883b8274a51
gbs2gps <gbs2gps@virginia.edu>
2021-12-15 17:26:00 -0500

    more edits

--- colors/views.py
+++ colors/views.py
@@ -8,9 +8,6 @@ def index(request):
 def index1(request):
     return render(request, 'colors/colorstuff1.html')
 
-def index2(request):
-    return render(request, 'colors/colorstuff2.html')
-
 def index3(request):
     return render(request, 'colors/colorstuff3.html')
 
@@ -28,5 +25,55 @@ def index7(request):
 
 def index8(request):
     return render(request, 'colors/colorstuff8.html')
+
+def index9(request):
+    return render(request, 'colors/squarestuff.html')
+
+def index10(request):
+    return render(request, 'colors/squarestuff1.html')
+
+def index11(request):
+    return render(request, 'colors/squarestuff2.html')
+
+def index13(request):
+    return render(request, 'colors/squarestuff4.html')
+
+def index14(request):
+    return render(request, 'colors/squarestuff5.html')
+
+def index15(request):
+    return render(request, 'colors/squarestuff6.html')
+
+def index16(request):
+    return render(request, 'colors/squarestuff7.html')
+
+def index17(request):
+    return render(request, 'colors/squarestuff8.html')
+
+def index18(request):
+    return render(request, 'colors/otherstuff.html')
+
+def index19(request):
+    return render(request, 'colors/otherstuff1.html')
+
+def index21(request):
+    return render(request, 'colors/otherstuff3.html')
+
+def index22(request):
+    return render(request, 'colors/otherstuff4.html')
+
+def index23(request):
+    return render(request, 'colors/otherstuff5.html')
+
+def index24(request):
+    return render(request, 'colors/otherstuff6.html')
+
+def index25(request):
+    return render(request, 'colors/otherstuff7.html')
+
+def index26(request):
+    return render(request, 'colors/otherstuff8.html')
+
+
 
 # Create your views here.
